4058c3b351442128fb1806605e643d84ca74dd50
Diego Fernandes <diego.schell.f@gmail.com>
2018-06-06 17:22:11 -0300

    Ajustes no linting

--- App.js
+++ App.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import React, { Component } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { RNCamera } from "react-native-camera";
 
 export default class App extends Component {
   takePicture = async () => {
     if (this.camera) {
       const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options)
+      const data = await this.camera.takePictureAsync(options);
+
       alert(data.uri);
     }
-  }
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <RNCamera
-            ref={camera => { this.camera = camera }}
-            style = {styles.preview}
-            type={RNCamera.Constants.Type.back}
-            autoFocus={RNCamera.Constants.AutoFocus.on}
-            flashMode={RNCamera.Constants.FlashMode.off}
-            permissionDialogTitle={'Permission to use camera'}
-            permissionDialogMessage={'We need your permission to use your camera phone'}
+          ref={camera => {
+            this.camera = camera;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          autoFocus={RNCamera.Constants.AutoFocus.on}
+          flashMode={RNCamera.Constants.FlashMode.off}
+          permissionDialogTitle={"Permission to use camera"}
+          permissionDialogMessage={
+            "We need your permission to use your camera phone"
+          }
         />
         <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={this.takePicture} style = {styles.capture}>
+          <TouchableOpacity onPress={this.takePicture} style={styles.capture}>
             <Text style={styles.buttonText}> SNAP </Text>
-        </TouchableOpacity>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -43,26 +42,26 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black'
+    flexDirection: "column",
+    backgroundColor: "black"
   },
   preview: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+    justifyContent: "flex-end",
+    alignItems: "center"
   },
   buttonContainer: {
     flex: 0,
-    flexDirection: 'row',
-    justifyContent: 'center'
+    flexDirection: "row",
+    justifyContent: "center"
   },
   capture: {
     flex: 0,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 5,
     padding: 15,
     paddingHorizontal: 20,
-    alignSelf: 'center',
+    alignSelf: "center",
     margin: 20
   },
   buttonText: {
