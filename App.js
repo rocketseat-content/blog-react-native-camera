f8cb9d0b3c83b08c64a60a7876ff3e619ab89c4f
Claudio Orlandi <cso.junior1996@gmail.com>
2018-06-06 16:40:30 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+export default class App extends Component {
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)
+      alert(data.uri);
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+            ref={camera => { this.camera = camera }}
+            style = {styles.preview}
+            type={RNCamera.Constants.Type.back}
+            autoFocus={RNCamera.Constants.AutoFocus.off}
+            flashMode={RNCamera.Constants.FlashMode.off}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={this.takePicture} style = {styles.capture}>
+            <Text style={styles.buttonText}> SNAP </Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  },
+  buttonText: {
+    fontSize: 14
+  }
+});
